a2aa0c317a2bf76dbba3cf1c4b4803c1a2c78738
MartynovOleg <103255306+MartynovOleg@users.noreply.github.com>
2022-04-14 22:18:37 +0300

    Initial Commit

---
+++ Armageddon 2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Armageddon 2022
+//
+//  Created by mac on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
